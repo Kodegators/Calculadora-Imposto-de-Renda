44a0455010c958ac3c4fe7209c4617754a79f239
JoaoOtavioOCortes <joaootavioocortes@gmail.com>
2024-09-03 19:31:15 -0300

    Correções

--- Calculadora Imposo de Renda/Program.cs
+++ Calculadora Imposo de Renda/Program.cs
@@ -1,4 +1,4 @@
-﻿using Calculadora_do_Leão_da_Federal.classes;
+﻿using Calculadora_Imposo_de_Renda.uteis;
 
 Console.WriteLine("Sejam Bem vindos ao seu adiantador de tempo na hora de pagar seus impostos, mesmo que seja roubo.");
 Console.WriteLine("0 - Sair\n1 - Soma\n2 - Conversor\n3 - Fazer Imposto de Renda\nO que deseja fazer?: ");
@@ -13,8 +13,6 @@ do
             break;
 
         case 1:
-            Receber();
-            var res = calculadora.Soma(num1, num2);
             break;
 
         case 2:
@@ -25,11 +23,3 @@ do
 
     }
 } while (opt != 0);
-
-void Receber()
-{
-    Console.Write("Digite o primeiro numero: ");
-    int num1 = int.Parse(Console.ReadLine());
-    Console.Write("Digite o segundo numero: ");
-    int num2 = int.Parse(Console.ReadLine());
-}
