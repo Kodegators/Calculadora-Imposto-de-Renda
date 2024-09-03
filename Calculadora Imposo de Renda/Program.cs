510ff051564cdd63ea732301f157c372981cb67a
JoaoOtavioOCortes <joaootavioocortes@gmail.com>
2024-09-03 20:21:03 -0300

    começando torre de hanoi

--- Calculadora Imposo de Renda/Program.cs
+++ Calculadora Imposo de Renda/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Calculadora_Imposo_de_Renda.uteis;
 
 Console.WriteLine("Sejam Bem vindos ao seu adiantador de tempo na hora de pagar seus impostos, mesmo que seja roubo.");
-Console.WriteLine("0 - Sair\n1 - Soma\n2 - Conversor\n3 - Fazer Imposto de Renda\nO que deseja fazer?: ");
+Console.WriteLine("0 - Sair\n1 - Fazer Imposto de Renda\n2 - Torres de Hanoi: ");
 int opt;
 do
 {
@@ -16,6 +16,7 @@ do
             break;
 
         case 2:
+            TorresHanoi.TorresHanoiGame();
             break;
 
         case 3:
---
+++ Calculadora Imposo de Renda/uteis/TorresHanoi.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora_Imposo_de_Renda.uteis
+{
+    internal class TorresHanoi
+    {
+        public static void TorresHanoiGame()
+        {
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine(
+                "            []                                []                                          []\n" +
+                "            []                                []                                          []\n" +
+                "            []                                []                                          []\n" +
+                "            []                                []                                          []\n" +
+                "            []                                []                                          []\n" +
+                "            []                                []                                          []\n" +
+                "            []                                []                                          []\n" +
+                "            []                                []                                          []\n" +
+                "            []                                []                                          []\n" +
+                "            []                                []                                          []\n" +
+                "        ------------                          []                                          []\n" +
+                "            []                                []                                          []\n" +
+                "            []                                []                                          []\n" +
+                "            []                                []                                          []\n" +
+                "            []                                []                                          []\n" +
+                "    ------------------                        []                                          []\n" +
+                "            []                                []                                          []\n" +
+                "            []                                []                                          []\n" +
+                "            []                                []                                          []\n");
+        }
+    }
+}
